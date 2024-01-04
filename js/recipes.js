96bee2cf1e4a193000fba7854fd55ce9f29a507b
kevinDronnn <voroshilin777@icloud.com>
2024-01-05 01:50:29 +0200

    added fetch for products and view

--- js/recipes.js
+++ js/recipes.js
@@ -1,1 +1,127 @@
+function openModal() {
+  document.getElementById("modal").style.display = "block";
+}
 
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+document.getElementById("add").addEventListener("click", function () {
+  openModal();
+});
+document.getElementById("closeModal").addEventListener("click", function () {
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+});
+
+document.getElementById("addFields").addEventListener("click", function () {
+  addFields();
+});
+
+function addFields() {
+  var lproductList = document.getElementById("l-productList");
+
+  var newInputLeft = document.createElement("input");
+  newInputLeft.setAttribute("type", "text");
+  newInputLeft.setAttribute("name", "products_name[]");
+  newInputLeft.setAttribute("placeholder", "name");
+  newInputLeft.classList.add("left");
+  newInputLeft.classList.add("modalField");
+
+  var newInputRight = document.createElement("input");
+  newInputRight.setAttribute("type", "text");
+  newInputRight.setAttribute("name", "products_grams[]");
+  newInputRight.setAttribute("placeholder", "grams");
+  newInputRight.classList.add("right");
+  newInputRight.classList.add("modalField");
+
+  var lileft = document.createElement("li");
+  lileft.classList.add("li-left");
+
+  lileft.appendChild(newInputLeft, newInputRight);
+  lileft.appendChild(newInputRight);
+  lproductList.appendChild(lileft);
+}
+
+$(document).ready(function () {
+  $("#submitButton").click(function () {
+    var formData = new FormData($("#combinedForm")[0]);
+
+    $(".li-left").each(function (index) {
+      var productName = $(this).find('input[name="products_name[]"]').val();
+      var productGrams = $(this).find('input[name="products_grams[]"]').val();
+
+      formData.append("products_name", productName);
+      formData.append("products_grams", productGrams);
+    });
+
+    $.ajax({
+      url: "http://localhost:8080/api/recipe",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (error) {
+        console.error("Error:", error);
+      },
+    });
+  });
+});
+
+const second_section = document.querySelector(".second-section");
+
+// function createElement(tag, className, textContent) {
+//   const element = document.createElement(tag);
+//   if (className) {
+//     element.classList.add(className);
+//   }
+//   if (textContent) {
+//     element.textContent = textContent;
+//   }
+//   return element;
+// }
+
+fetch("http://localhost:8080/api/recipe")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((product) => {
+      const container = document.createElement("div");
+      container.classList.add("container", "second-section__container");
+      container.setAttribute("id", "foodContainer");
+
+      const list = document.createElement("ul");
+      list.classList.add("second-section__food-box");
+      const list_item = document.createElement("li");
+      list_item.classList.add("second-section__food-item", "food-item");
+      const naming = product.image;
+
+      const extension = naming.substring(naming.lastIndexOf("\\"));
+
+      const list_img = document.createElement("img", "food-item__img");
+      list_img.setAttribute("src", "images/Recipces Page/" + extension);
+      list_img.setAttribute("height", "370");
+      list_img.setAttribute("width", "370");
+      list_img.classList.add("food-item__img");
+
+      const list_header = document.createElement("h3");
+      list_header.classList.add("food-item__title");
+      const list_par = document.createElement("p");
+      list_par.classList.add("food-item__text");
+
+      list_header.innerText = product.name;
+      list_par.innerText = product.description;
+      list_item.appendChild(list_img);
+      list_item.appendChild(list_header);
+      list_item.appendChild(list_par);
+
+      list.appendChild(list_item);
+      container.appendChild(list);
+      second_section.appendChild(container);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
